34238d630c694300790776de09048285ecca1e5e
Qunfei Wu <wu.qunfei@gmail.com>
2020-12-27 09:07:59 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,8 @@
 """This module contains the packaging routine for the pybook package"""
 
 from setuptools import setup, find_packages
-try:
-    from pip.download import PipSession
-    from pip.req import parse_requirements
-except ImportError:
-    # It is quick hack to support pip 10 that has changed its internal
-    # structure of the modules.
-    from pip._internal.download import PipSession
-    from pip._internal.req.req_file import parse_requirements
+from pip._internal.network.session import PipSession
+from pip._internal.req import parse_requirements
 
 
 def get_requirements(source):
